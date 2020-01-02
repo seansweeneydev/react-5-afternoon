86a95d8d3764f7e90ea73e5e848006a60e7ee298
Sean Sweeney <seansweeney@Seans-MacBook-Pro.local>
2020-01-02 10:57:36 -0700

    updated

--- src/components/WizardEleven/WizardEleven.js
+++ src/components/WizardEleven/WizardEleven.js
@@ -1,8 +1,8 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class WizardEleven extends Component {
-
     render(){
         return(
             <div className="parent-div">
@@ -126,4 +126,25 @@ class WizardEleven extends Component {
     }
 }
 
-export default WizardEleven;
+function mapStateToProps( state ) {
+    return{ 
+        loanType: state.loanType,
+        propertyType: state.propertyType,
+        city: state.city,
+        propToBeUsedOn: state.propToBeUsedOn,
+        found: state.found,
+        realEstateAgent: state.realEstateAgent,
+        cost: state.cost,
+        downPayment: state.downPayment,
+        credit: state.credit,
+        history: state.history,
+        addressOne: state.addressOne,
+        addressTwo: state.addressTwo,
+        addressThree: state.addressThree,
+        firstName: state.firstName,
+        lastName: state.lastName,
+        email: state.email
+    }
+}
+
+export default connect(mapStateToProps)(WizardEleven);
--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,106 @@
-function reducer(){
-
+const initialState = {
+    loanType: 'Home Purchase',
+    propertyType: 'Single Family Home',
+    city: '',
+    propToBeUsedOn: '',
+    found: "false",
+    realEstateAgent: "false",
+    cost: 0,
+    downPayment: 0,
+    credit: '',
+    history: '',
+    addressOne: '',
+    addressTwo: '',
+    addressThree: '',
+    firstName: '',
+    lastName: '',
+    email: ''
 }
+
+// CREATING ACTION TYPES 
+const UPDATE_LOAN_TYPE = 'UPDATE_LOAN_TYPE'
+const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE'
+const UPDATE_CITY = 'UPDATE_CITY'
+const UPDATE_PROP = 'UPDATE_PROP'
+const UPDATE_FOUND = 'UPDATE_FOUND'
+
+// CREATING ACTION CREATORS 
+export function updateLoanType( loanType ) {
+    return {
+        type: UPDATE_LOAN_TYPE,
+        payload: loanType
+    }
+}
+
+export function updatePropertyType( property ) {
+    return {
+        type: UPDATE_PROPERTY_TYPE,
+        payload: property
+    }
+}
+
+export function updateCity( city ) {
+    return {
+        type: UPDATE_CITY,
+        payload: city
+    }
+}
+
+export function updateProp( prop ) {
+    return {
+        type: UPDATE_PROPERTY_TYPE,
+        payload: prop
+    }
+} 
+
+export function updateFound( found ) {
+    return {
+        type: UPDATE_FOUND,
+        payload: found
+    }
+}
+
+// MODIFYING REDUCER FUNCTION WITH STATE AND ACTION
+// STATE DEFAULTS TO INITIAL STATE
+function reducer(state = initialState, action) {
+    console.log('REDUCER HIT: Action ->', action);
+
+    switch(action.type) {
+        case UPDATE_LOAN_TYPE:
+            return {...state, loanType: action.payload}
+        case UPDATE_PROPERTY_TYPE:
+            return {...state, propertyType: action.payload}
+        case UPDATE_CITY:
+            return {...state, city: action.payload}
+        case UPDATE_PROP:
+            return{...state, propToBeUsedOn: action.payload}
+        case UPDATE_FOUND:
+            return{...state, found: action.payload}
+                
+        default: return state;
+    }
+}
+// function reducer( state = initialState, action ) { 
+//     console.log('REDUCER HIT: Action ->', action );
+    
+//     switch( action.type ){
+//       case UPDATE_LOAN_TYPE:
+//         return Object.assign( {}, state, { loanType: action.payload } );
+
+//       case UPDATE_PROPERTY_TYPE:
+//         return Object.assign( {}, state, { propertyType: action.payload } );
+
+//       case UPDATE_CITY:
+//         return Object.assign( {}, state, { city: action.payload } );
+
+//       case UPDATE_PROP:
+//         return Object.assign( {}, state, { propToBeUsedOn: action.payload } );
+
+//       case UPDATE_FOUND:
+//         return Object.assign( {}, state, { found: action.payload } );
+
+//       default: return state;
+//     }
+// }
 
 export default reducer;
